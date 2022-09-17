50a06d7c8c29e045827a8d3216f3dc0e73257087
Matt-JL-Burton <mat@emux.app>
2022-09-17 17:38:56 +0100

    added day 2 of maths to repo

---
+++ Maths/handshakes2.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'handshake' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def handshake(n):
+    count = 0
+    for i in range(n):
+        count = count + i
+        # fptr.write(str(i) + '\n')
+    return (count)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = handshake(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
