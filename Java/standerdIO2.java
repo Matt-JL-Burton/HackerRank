336c2b6bb65947656363b51d36dad82195521457
Matt-JL-Burton <mat@emux.app>
2022-09-29 22:27:29 +0100

    added Java solutions for practise Neither work not on the hacker rank env yet

---
+++ Java/standerdIO2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstInt = scanner.nextInt();
+        int secondInt = scanner.nextInt();
+        int thirdInt = scanner.nextInt();
+        System.out.println(firstInt);
+        System.out.println(secondInt);
+        System.out.println(thirdInt);  
+    }
+}
