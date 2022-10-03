dd782e8b3621dabf0ee23fe7c1fb27e6d3834673
Matt-JL-Burton <mat@emux.app>
2022-10-03 23:57:42 +0100

    succesfully added num 3

--- Java/ifelse3.java
+++ Java/ifelse3.java
@@ -1,3 +1,20 @@
-public class ifelse3 {
-    
+import java.io.*;
+
+
+public class ifelse3{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        if (N%2 == 1){
+            System.out.println("Weird");
+        } else if (N<=5 && N>=2){
+            System.out.println("Not Weird");
+        } else if (N<=20 && N>=6){
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+        bufferedReader.close();
+    }
 }
