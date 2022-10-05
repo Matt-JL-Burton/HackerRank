c509ece56ed5134ef8c0ef862781b60653c4630e
Matt-JL-Burton <mat@emux.app>
2022-10-05 21:49:11 +0100

    other minor cahgnes

---
+++ Java/outputFormatting5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class outputFormatting5 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstLineTxt = scanner.next();
+        int firstLineInt = scanner.nextInt();
+        String secondLineTxt = scanner.next();
+        int secondLineInt = scanner.nextInt();        
+        String thirdLineTxt = scanner.next();
+        int thirdLineInt = scanner.nextInt();
+        System.out.printf("================================\n");
+        System.out.printf("%-14s %03d\n",firstLineTxt,firstLineInt);
+        System.out.printf("%-14s %03d\n",secondLineTxt,secondLineInt);
+        System.out.printf("%-14s %03d\n",thirdLineTxt,thirdLineInt);
+        System.out.printf("================================\n");
+        scanner.close();
+    }
+    
+}
