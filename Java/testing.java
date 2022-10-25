df9bd2c0e90c65cac6847fe1e6bfc70d9a59ed5c
Matt-JL-Burton <mat@emux.app>
2022-10-25 15:48:19 +0100

    Found my java doesn't merge equal strings to identiical when strings are user inputted

--- Java/testing.java
+++ Java/testing.java
@@ -1,9 +1,18 @@
-import java.util.ArrayList;
+import java.util.Scanner;
+
 public class testing{
     
-    static void main(String[] args) {
-        ArrayList<String> y = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String x = "Hello";
+        String y = scanner.nextLine();
         System.out.println(y);
+        if (x == y){
+            System.out.println("Same");
+        } else {
+            System.out.println("Not the smae");
+        }
+        scanner.close();
 
     }
     
